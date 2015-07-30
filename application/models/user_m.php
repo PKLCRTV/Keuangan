e7f4cd3f9a8b2d1e4c08efb1cf28aee782c6feb8
rizakbar2 <rizaelektro12@gmail.com>
2015-07-30 09:37:30 +0700

    keuangan v.1.0

---
+++ application/models/user_m.php
@@ -0,0 +1,40 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class User_m extends CI_Model
+{
+	public $tabel = "tbl_user";
+	public $kode = "id_user";
+
+	function selectAll()
+	{
+		$query = $this->db->get($this->$tabel);
+		return $query->result();
+	}
+
+	function selectBy($field, $isi)
+	{
+		$this->db->where($field, $isi);
+		$query = $this->db->get($this->$tabel);
+		return $query->result();
+	}
+
+	function insert($data)
+	{
+		$query = $this->db->insert($this->$tabel, $data);
+		return $query;
+	}
+
+	function update($data, $id)
+	{
+		$this->db->where($this->$kode, $id);
+		$query = $this->db->update($this->$tabel,$data);
+		return $query;
+	}
+
+	function delete($id)
+	{
+		$this->db->where($this->$kode,$id);
+		$query = $this->db->delete($this->$tabel);
+		return $query;
+	}
+}
